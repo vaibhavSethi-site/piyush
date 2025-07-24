6b9d6220b59d66a93697d6c6c9a1f399aaf3e6ad
vaibhavSethi-site <99320550+vaibhavSethi-site@users.noreply.github.com>
2025-07-24 17:05:21 +0530

    Delete frontend directory

--- frontend/src/components/useMount.jsx
+++
@@ -1,9 +0,0 @@
-import { useEffect } from 'react';
-
-const useMountEffect = (effect) => {
-  useEffect(() => {
-    effect();
-  }, []); // Empty dependency array ensures the effect runs only once on mount
-};
-
-export default useMountEffect;
